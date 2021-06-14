8d98485676184572a18edde88c5582787cec707f
hieuap28 <hieuap28@gmail.com>
2021-06-14 11:47:19 +0700

    xong qlroom

---
+++ src/main/java/com/example/service/IRoomService.java
@@ -0,0 +1,12 @@
+package com.example.sevice;
+
+import com.example.model.dto.RoomDTO;
+
+import java.util.List;
+
+public interface IRoomService {
+    public List<RoomDTO> findAll();
+    public RoomDTO findById(long id);
+    public RoomDTO save(RoomDTO r);
+    public boolean delete(long id);
+}
